713ae3d37b1b6adcc45c9d7e55292cff67bef16a
tea <teacplusplus@gmail.com>
2015-04-06 15:43:52 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaitZnakomstv::Application.config.secret_token = '96eed02119a8c9c6a4e668bb1211beefc0304a5f27887fa703e4d83fec43b1cb600df89ce44768638cfda7c5eb4c1dd8a47e6836daa0174d8fe4d6e8b4c8ced4'
